02531433406468cac48f992717e0777028fc5d20
Lasse Skindstad Ebert <lasse@lasseebert.dk>
2024-12-03 07:56:35 +0100

    Day 3 part 2

--- lib/advent/day_03.ex
+++ lib/advent/day_03.ex
@@ -10,15 +10,12 @@ defmodule Advent.Day03 do
   def part_1(input) do
     input
     |> parse()
-    |> scan_mul()
-    |> Enum.map(fn {a, b} -> a * b end)
+    |> Enum.filter(fn
+      {:mul, _a, _b} -> true
+      _ -> false
+    end)
+    |> Enum.map(fn {:mul, a, b} -> a * b end)
     |> Enum.sum()
-  end
-
-  defp scan_mul(input) do
-    ~r/mul\((\d{1,3}),(\d{1,3})\)/
-    |> Regex.scan(input, capture: :all_but_first)
-    |> Enum.map(fn [a, b] -> {String.to_integer(a), String.to_integer(b)} end)
   end
 
   @doc """
@@ -28,14 +25,25 @@ defmodule Advent.Day03 do
   def part_2(input) do
     input
     |> parse()
-
-    0
+    |> Enum.reduce({0, true}, fn
+      {:mul, a, b}, {acc, true} -> {acc + a * b, true}
+      {:mul, _a, _b}, {acc, false} -> {acc, false}
+      :do, {acc, _} -> {acc, true}
+      :dont, {acc, _} -> {acc, false}
+    end)
+    |> then(fn {sum, _} -> sum end)
   end
 
   defp parse(input) do
-    input
-    |> String.trim()
-    |> String.split("\n", trim: true)
-    |> Enum.join("")
+    input = input |> String.trim()
+
+    ~r/(mul)\((\d{1,3}),(\d{1,3})\)|(do)\(\)|(don't)\(\)/
+    |> Regex.scan(input, capture: :all_but_first)
+    |> Enum.map(fn captures -> Enum.filter(captures, &(&1 != "")) end)
+    |> Enum.map(fn
+      ["mul", a, b] -> {:mul, String.to_integer(a), String.to_integer(b)}
+      ["do"] -> :do
+      ["don't"] -> :dont
+    end)
   end
 end
--- test/advent/day_03_test.exs
+++ test/advent/day_03_test.exs
@@ -3,15 +3,19 @@ defmodule Advent.Day03Test do
 
   alias Advent.Day03
 
-  @example_input """
+  @example_input_1 """
   xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
+  """
+
+  @example_input_2 """
+  xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
   """
 
   @puzzle_input File.read!("puzzle_inputs/day_03.txt")
 
   describe "part 1" do
     test "example" do
-      assert Day03.part_1(@example_input) == 161
+      assert Day03.part_1(@example_input_1) == 161
     end
 
     @tag :puzzle_input
@@ -21,15 +25,13 @@ defmodule Advent.Day03Test do
   end
 
   describe "part 2" do
-    @tag :skip
     test "example" do
-      assert Day03.part_2(@example_input) == :foo
+      assert Day03.part_2(@example_input_2) == 48
     end
 
-    @tag :skip
     @tag :puzzle_input
     test "puzzle input" do
-      assert Day03.part_2(@puzzle_input) == :foo
+      assert Day03.part_2(@puzzle_input) == 70_478_672
     end
   end
 end
